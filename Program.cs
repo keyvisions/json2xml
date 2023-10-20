76ecc942b5b4fab85a43cd85f365e3de02e43722
Giancarlo Trevisan <g.trevisan@keyvisions.it>
2023-10-20 07:25:54 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Text.RegularExpressions;
+using System.Security; 
+
+namespace json2xml
+{
+    class Program
+    {
+        // https://json.org/json-it.html
+        private static string _json = "";
+        private static string json2xml(string json, string name = "root")
+        {
+            Regex regex = new Regex(@"^\s*\{\s*""(?<name>[a-z0-9_]+?)""\s*:\s*(?<rest>[\s\S]*)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            Match match = regex.Match(json);
+            if (match.Success)
+                return String.Format("<{0}>{1}</{0}>", name == "" ? "element" : name, json2xml(match.Groups["rest"].Value, match.Groups["name"].Value)) + json2xml(_json, "element");
+
+            regex = new Regex(@"^\s*\[\s*(?<rest>[\s\S]*)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            match = regex.Match(json);
+            if (match.Success)
+                return String.Format("<{0}>{1}</{0}>", name, json2xml(match.Groups["rest"].Value, match.Groups["name"].Value)) + json2xml(_json, "element");
+
+            regex = new Regex(@"^""(?<name>[a-z0-9_]+?)""\s*:\s*(?<rest>[\s\S]*)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            match = regex.Match(json);
+            if (match.Success)
+                return json2xml(match.Groups["rest"].Value, match.Groups["name"].Value);
+
+            regex = new Regex(@"^(?<value>true|false|null|-?(?:0|[1-9])[0-9]*(?:\.[0-9]+)?(?:e[\-+]?[0-9]+)?|""(?:\\""|.)*?"")\s*,?\s*(?<rest>[\s\S]*)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            match = regex.Match(json);
+            if (match.Success) {
+                if (name == "" || name == "root")
+                    name = "value";
+                string value = match.Groups["value"].Value.Trim('\"');
+                if (value == "null")
+                    value = "";
+                return String.Format("<{0}>{1}</{0}>", name, SecurityElement.Escape(value)) + json2xml(match.Groups["rest"].Value);
+            }
+
+            regex = new Regex(@"^(?:[\]}]\s*,?\s*)(?<rest>[\s\S]*)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            match = regex.Match(json);
+            if (match.Success)
+                _json = match.Groups["rest"].Value;
+
+            return "";
+        }
+        static void Main(string[] args)
+        {
+            string json = @"
+                { 
+                    ""image1"": "".\\media\\logo_dark.png"",
+                    ""PurchaseOrderNumber"": 99503,
+                    ""OrderDate"": ""1999-10-20"",
+                    ""Shipping"": {
+                        ""Name"": ""Ellen Adams"",
+                        ""Street"": ""123 Maple Street""
+                    }
+                }
+            ";
+
+            string xml = json2xml(json);
+          
+            Console.Write(xml);
+        }
+    }
+}

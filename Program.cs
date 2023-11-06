063abee9b148db97d4e7a9e38b42bd9771642fa4
Giancarlo Trevisan <g.trevisan@keyvisions.it>
2023-11-06 11:34:07 +0100

    x30 Speed-up static readonly Regex

--- Program.cs
+++ Program.cs
@@ -8,13 +8,18 @@ namespace json2xml
     class Program
     {
         private static string _json = "";
+        private static readonly Regex regex_name = new Regex(@"^\s*\{\s*""(?<name>[a-z0-9_]+?)""\s*:\s*(?<rest>[\s\S]*)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private static readonly Regex regex_rest = new Regex(@"^\s*\[\s*(?<rest>[\s\S]*)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private static readonly Regex regex_name_rest = new Regex(@"^""(?<name>[a-z0-9_]+?)""\s*:\s*(?<rest>[\s\S]*)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private static readonly Regex regex_value = new Regex(@"^(?<value>true|false|null|-?(?:0|[1-9])[0-9]*(?:\.[0-9]+)?(?:e[\-+]?[0-9]+)?|""(?:\\""|.)*?"")\s*,?\s*(?<rest>[\s\S]*)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private static readonly Regex regex_final = new Regex(@"^(?:[\]}]\s*,?\s*)(?<rest>[\s\S]*)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
         private static string json2xml(string json, string name = "root")
         {
             if (json == "")
                 return "";
 
-            Regex regex = new Regex(@"^\s*\{\s*""(?<name>[a-z0-9_]+?)""\s*:\s*(?<rest>[\s\S]*)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-            Match match = regex.Match(json);
+            Match match = regex_name.Match(json);
             if (match.Success)
             {
                 if (name != "")
@@ -22,8 +27,7 @@ namespace json2xml
                 return json2xml(match.Groups["rest"].Value, match.Groups["name"].Value) + json2xml(_json, "");
             }
 
-            regex = new Regex(@"^\s*\[\s*(?<rest>[\s\S]*)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-            match = regex.Match(json);
+            match = regex_rest.Match(json);
             if (match.Success)
             {
                 if (name != "")
@@ -31,13 +35,11 @@ namespace json2xml
                 return json2xml(match.Groups["rest"].Value, match.Groups["name"].Value) + json2xml(_json, "");
             }
 
-            regex = new Regex(@"^""(?<name>[a-z0-9_]+?)""\s*:\s*(?<rest>[\s\S]*)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-            match = regex.Match(json);
+            match = regex_name_rest.Match(json);
             if (match.Success)
                 return json2xml(match.Groups["rest"].Value, match.Groups["name"].Value);
 
-            regex = new Regex(@"^(?<value>true|false|null|-?(?:0|[1-9])[0-9]*(?:\.[0-9]+)?(?:e[\-+]?[0-9]+)?|""(?:\\""|.)*?"")\s*,?\s*(?<rest>[\s\S]*)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-            match = regex.Match(json);
+            match = regex_value.Match(json);
             if (match.Success)
             {
                 if (name == "" || name == "root")
@@ -48,8 +50,7 @@ namespace json2xml
                 return String.Format("<{0}>{1}</{0}>", name, SecurityElement.Escape(value)) + json2xml(match.Groups["rest"].Value);
             }
 
-            regex = new Regex(@"^(?:[\]}]\s*,?\s*)(?<rest>[\s\S]*)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-            match = regex.Match(json);
+            match = regex_final.Match(json);
             if (match.Success)
             {
                 _json = match.Groups["rest"].Value;
@@ -61,8 +62,9 @@ namespace json2xml
 
         static void Main(string[] args)
         {
+#if DEBUG
             var watch = System.Diagnostics.Stopwatch.StartNew();
-
+#endif
             string json = "";
 
             StreamReader sr = new StreamReader("data.json");
@@ -75,9 +77,10 @@ namespace json2xml
             {
                 outputFile.WriteLine(xml);
             }
-
+#if DEBUG
             watch.Stop();
             Console.WriteLine($"Time: {watch.ElapsedMilliseconds} ms");
+#endif
         }
     }
 }
